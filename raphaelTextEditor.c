edf068cb1bbe6308e4b9fcdac2876d7afe870081
Raphael Napinas <rcnapinas@cpp.edu>
2021-12-05 20:04:22 -0800

    Completed up to step 41, need to do 42.

--- raphaelTextEditor.c
+++ raphaelTextEditor.c
@@ -4,11 +4,14 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
+
+#define RAPHAEL_EDITOR_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -81,18 +84,17 @@ int getCursorPosition(int *rows, int *cols)
   }
   buf[i] = '\0';
 
-  printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
-  editorReadKey();
-
-  return -1;
+  return 0;
 }
 
 int getWindowSize(int *rows, int *cols)
 {
   struct winsize ws;
 
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
   {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != ) return -1;
     return getCursorPosition(rows, cols);
@@ -105,25 +107,75 @@ int getWindowSize(int *rows, int *cols)
   }
 }
 
+/*** appned buffer ***/
+
+struct abuf
+{
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+  free(ab->b);
+}
+
 /*** output **/
 
-void editorDrawRows()
+void editorDrawRows(struct abuf *ab)
 {
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    if (y == < E.screenrows; y++)
+    {
+      if (y == E.screenrows / 3)
+      {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome), "Raphael Text Editor -- version %s", RAPHAEL_EDITOR_VERSION);
+        if (welcomelen, > E.screencols) welcomelen = E.screencols;
+        abAppend(ab, welcome, welcomelen);
+      }
+      else
+      {
+        abAppend(ab, "~", 1);
+      }
+    }
+
+    abAppend(ab, "\x1b[K", 3);
+    if (y < E.screenrows - 1)
+    {
+      abAppend(ab, "\r\n", 2);
+    }
   }
 }
 
 void editorRefreshScreen()
 {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 /*** input **/
