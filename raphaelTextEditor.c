c29552207126f2dfbd45c41cc4f3cd81f4f92e64
Raphael Napinas <rcnapinas@cpp.edu>
2021-12-05 20:36:29 -0800

    Finished up to step 47. Need to do 48.

--- raphaelTextEditor.c
+++ raphaelTextEditor.c
@@ -15,9 +15,18 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 'a',
+  ARROW_RIGHT = 'd',
+  ARROW_UP = 'w',
+  ARROW_DOWN = 's'
+};
+
 /*** data ***/
+
 struct editorConfig
 {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -66,7 +75,30 @@ char editorReadKey()
   {
     if (nread == -1 && errno != EAGAIN) die ("read");
   }
-  return c;
+
+  if (c == '\x1b')
+  {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if (seq[0] == '[')
+    {
+      switch (seq[1])
+      {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  }
+  else
+  {
+    return c;
+  }
 }
 
 int getCursorPosition(int *rows, int *cols)
@@ -144,6 +176,13 @@ void editorDrawRows(struct abuf *ab)
       char welcome[80];
       int welcomelen = snprintf(welcome, sizeof(welcome), "Raphael Text Editor -- version %s", RAPHAEL_EDITOR_VERSION);
       if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding)
+      {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
       abAppend(ab, welcome, welcomelen);
     }
     else
@@ -168,7 +207,10 @@ void editorRefreshScreen()
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -176,6 +218,25 @@ void editorRefreshScreen()
 }
 
 /*** input **/
+
+void editorMoveCursor(char key)
+{
+  switch (key)
+  {
+    case ARROW_LEFT:
+      E.cx--;
+      break;
+    case ARROW_RIGHT:
+      E.cx++;
+      break;
+    case ARROW_UP:
+      E.cy--;
+      break;
+    case ARROW_DOWN:
+      E.cy++;
+      break;
+  }
+}
 
 void editorProcessKeypress()
 {
@@ -188,6 +249,13 @@ void editorProcessKeypress()
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+      case ARROW_UP:
+      case ARROW_DOWN:
+      case ARROW_LEFT:
+      case ARROW_RIGHT:
+        editorMoveCursor(c);
+        break;
   }
 }
 
@@ -195,6 +263,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
