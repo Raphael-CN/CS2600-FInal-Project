6fcf957dfa73bf5092dba89f1ddbf2dcdb9399b9
Raphael Napinas <rcnapinas@cpp.edu>
2021-12-07 14:13:30 -0800

    Completed up to step 147, need to do 148

--- raphaelTextEditor.c
+++ raphaelTextEditor.c
@@ -38,6 +38,12 @@ enum editorKey {
   PAGE_DOWN
 };
 
+enum editorHighlight
+{
+  HL_NORMAL = 0,
+  HL_NUMBER
+};
+
 /*** data ***/
 
 typedef struct erow
@@ -46,6 +52,7 @@ typedef struct erow
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 } erow;
 
 struct editorConfig
@@ -207,6 +214,32 @@ int getWindowSize(int *rows, int *cols)
   }
 }
 
+/*** syntax highlighting ***/
+
+void editorUpdateSyntax(erow *row)
+{
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+
+  int i;
+  for (i = 0; i < row->rsize; i++)
+  {
+    if (isdigit(row->render[i]))
+    {
+      row->hl[i] = HL_NUMBER;
+    }
+  }
+}
+
+int editorSyntaxToColor(int hl)
+{
+  switch (hl)
+  {
+    case HL_NUMBER: return 31;
+    default: return 37;
+  }
+}
+
 /*** row operations ***/
 
 int editorRowCxToRx(erow *row, int cx)
@@ -263,6 +296,8 @@ void editorUpdateRow(erow *row)
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len)
@@ -279,6 +314,7 @@ void editorInsertRow(int at, char *s, size_t len)
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -289,6 +325,7 @@ void editorFreeRow(erow *row)
 {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at)
@@ -621,7 +658,21 @@ void editorDrawRows(struct abuf *ab)
       int len = E.row[filerow].rsize - E.coloff;
       if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, &E.row[filerow].render[E.coloff], len);
+      char *c = &E.row[filerow].render[E.coloff];
+      int j;
+      for (j = 0; j < len; j++)
+      {
+        if (isdigit(c[j]))
+        {
+          abAppend(ab, "\x1b[31m", 5);
+          abAppend(ab, &c[j], 1);
+          abAppend(ab, "\x1b[39m", 5);
+        }
+        else
+        {
+          abAppend(ab, &c[j], 1);
+        }
+      }
     }
 
     abAppend(ab, "\x1b[K", 3);
